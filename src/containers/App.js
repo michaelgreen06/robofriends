ac33c14a4007560cd36598c1a91b1e528976af83
Michael Green <michaelgreen06@gmail.com>
2023-08-30 10:00:10 -0600

    converted app to use hooks!

--- src/containers/App.js
+++ src/containers/App.js
@@ -7,6 +7,7 @@ import "./App.css";
 //my try
 //8/28 left off figuring out where the props in the card component are passed in
 //8/29 keep exploring how this app works. was having trouble w/ destructurring on 8/28
+//8/30 0847 start
 
 //useEffect will be used whenever the searchfield changes.
 
@@ -23,7 +24,7 @@ export default function App() {
   }, []);
 
   function handleChange(e) {
-    setSearchfield({ searchfield: e.target.value });
+    setSearchfield(e.target.value);
   }
 
   const filteredRobots = robots.filter((robot) => {
@@ -43,47 +44,47 @@ export default function App() {
 }
 
 ////////////////
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      robots: [],
-      searchfield: "",
-    };
-  }
+// class App extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       robots: [],
+//       searchfield: "",
+//     };
+//   }
 
-  componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((response) => response.json())
-      .then((users) => {
-        this.setState({ robots: users });
-      });
-  }
+//   componentDidMount() {
+//     fetch("https://jsonplaceholder.typicode.com/users")
+//       .then((response) => response.json())
+//       .then((users) => {
+//         this.setState({ robots: users });
+//       });
+//   }
 
-  //this creates a class method. it updates the searchfield value in the components state
-  //it is using arrow notation becauase it automatically binds to this. if it were using traditional function
-  //notation we would have to explicitly bind it.
-  onSearchChange = (event) => {
-    this.setState({ searchfield: event.target.value });
-  };
+//   //this creates a class method. it updates the searchfield value in the components state
+//   //it is using arrow notation becauase it automatically binds to this. if it were using traditional function
+//   //notation we would have to explicitly bind it.
+//   onSearchChange = (event) => {
+//     this.setState({ searchfield: event.target.value });
+//   };
 
-  render() {
-    const { robots, searchfield } = this.state;
-    const filteredRobots = robots.filter((robot) => {
-      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
-    });
-    return !robots.length ? (
-      <h1>Loading</h1>
-    ) : (
-      <div className="tc">
-        <h1 className="f1">RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <Scroll>
-          <CardList robots={filteredRobots} />
-        </Scroll>
-      </div>
-    );
-  }
-}
+//   render() {
+//     const { robots, searchfield } = this.state;
+//     const filteredRobots = robots.filter((robot) => {
+//       return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+//     });
+//     return !robots.length ? (
+//       <h1>Loading</h1>
+//     ) : (
+//       <div className="tc">
+//         <h1 className="f1">RoboFriends</h1>
+//         <SearchBox searchChange={this.onSearchChange} />
+//         <Scroll>
+//           <CardList robots={filteredRobots} />
+//         </Scroll>
+//       </div>
+//     );
+//   }
+// }
 
 // export default App;
